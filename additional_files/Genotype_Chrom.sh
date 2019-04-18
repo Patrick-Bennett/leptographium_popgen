f992eec569a8805d1f8d24567e55169b90136dcb
Tabima <caifaz01@gmail.com>
2019-04-18 14:05:55 -0700

    1. Added scripts for post-gVCF processing: - `Combine_vcf.sh`: Combines gVCF from GATK, replaces Final_vcf.sh - `Genotype_Chrom.sh`: Generates population-wide genotypes per chromosome for the combined gVCF from `Combine_vcf.sh`
    2. Modified README.md from scripts/

---
+++ additional_files/Genotype_Chrom.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#$ -V
+#$ -v TMPDIR=/data
+#$ -N Geno_chrom
+#$ -l mem_free=40G
+#$ -S /bin/bash
+#$ -cwd
+#$ -t 1-122
+#$ -tc 10
+
+mkdir -p genotyped
+
+i=$(expr $SGE_TASK_ID)
+
+CMD="~/bin/gatk-4.0.1.2/gatk --java-options '-Xmx40g -Djava.io.tmpdir=/data -XX:ParallelGCThreads=1' GenotypeGVCFs -R /nfs1/BPP/LeBoldus_Lab/user_folders/bennetpa/BSRD_popgen/CMW154.fa -L scaffold_$SGE_TASK_ID -V Leptographium_2019.gvcf.gz -new-qual -O genotyped/Lepto.$SGE_TASK_ID.vcf.gz"
+
+echo $CMD
+eval $CMD
