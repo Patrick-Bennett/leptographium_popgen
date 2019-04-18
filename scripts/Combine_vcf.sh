e396f2abd3dcaf0d9d2b5b1a2e66633bddcc087f
Tabima <caifaz01@gmail.com>
2019-04-18 15:11:43 -0700

    Changed the relative to absolute paths

--- scripts/Combine_vcf.sh
+++ scripts/Combine_vcf.sh
@@ -11,7 +11,7 @@ i=$(expr $SGE_TASK_ID - 1)
 
 REF="/nfs1/BPP/LeBoldus_Lab/user_folders/bennetpa/BSRD_popgen/CMW154.fa"
 
-CMD="~/bin/gatk-4.0.1.2/gatk CombineGVCFs -R $REF -V gvcf.list  -O Leptographium_2019.gvcf.gz"
+CMD="/raid1/home/bpp/tabimaj/bin/gatk-4.0.1.2/gatk CombineGVCFs -R $REF -V gvcf.list  -O Leptographium_2019.gvcf.gz"
 echo $CMD
 eval $CMD
 
